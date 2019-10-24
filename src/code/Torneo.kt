1094bd29356bee2777d7cfbb9fef711b8b3da988
Nestor <nestor2502@ciencias.unam.mx>
2019-10-24 04:31:32 -0500

    se termina torneo, solamente hace falta adaptar un arreglo para la interfaz

--- src/code/Torneo.kt
+++ src/code/Torneo.kt
@@ -1,19 +1,41 @@
 package code
 import java.util.Random
-import code.Team
-class Torneo {
-    //Equipos participantes
-    private val equipo1 = Team("Team Liquid", generaHabilidad())
-    private val equipo2 = Team("Hong Kong Attitude", generaHabilidad())
-    private val equipo3 = Team("G2 Esports", generaHabilidad())
-    private val equipo4 = Team("Fnatic", generaHabilidad())
-    private val equipo5 = Team("Griffin", generaHabilidad())
-    private val equipo6 = Team("SK Telecom T1", generaHabilidad())
-    private val equipo7 = Team("Royal Never Give Up", generaHabilidad())
-    private val equipo8 = Team("Invictus Gaming", generaHabilidad())
-    private val equipos = arrayOf(equipo1, equipo2, equipo3, equipo4, equipo5, equipo6, equipo7, equipo8)
-    private var equipos_listos : MutableList<Team> = arrayListOf()//lista de equipos revueltos
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.*
 
+class Torneo constructor(saldo_inicial: Int){
+
+    //Equipos participantes
+    private val equipo1 : Team
+    private val equipo2 : Team
+    private val equipo3 : Team
+    private val equipo4 : Team
+    private val equipo5 : Team
+    private val equipo6 : Team
+    private val equipo7 : Team
+    private val equipo8 : Team
+    private val equipos : Array<Team>
+    private var equipos_listos : MutableList<Team>
+    private var saldo_inicial : Int
+    var saldo_generadoProperty = SimpleIntegerProperty(0)
+    private var saldo_generado by saldo_generadoProperty
+
+
+    init {
+        equipo1 = Team("Team Liquid", generaHabilidad())
+        equipo2 = Team("Hong Kong Attitude", generaHabilidad())
+        equipo3 = Team("G2 Esports", generaHabilidad())
+        equipo4 = Team("Fnatic", generaHabilidad())
+        equipo5 = Team("Griffin", generaHabilidad())
+        equipo6 = Team("SK Telecom T1", generaHabilidad())
+        equipo7 = Team("Royal Never Give Up", generaHabilidad())
+        equipo8 = Team("Invictus Gaming", generaHabilidad())
+        equipos = arrayOf(equipo1, equipo2, equipo3, equipo4, equipo5, equipo6, equipo7, equipo8)
+        equipos_listos = arrayListOf()//lista de equipos revueltos
+        this.saldo_inicial  = saldo_inicial
+        saldo_generado = 0 //saldo que se genera durante el torneo
+    }
     /**
      * Metodo que organiza los combates de manera aleatoria
      */
@@ -29,20 +51,24 @@ class Torneo {
         }
     }
 
+    /**
+     * Metodo auxiliar para ver a los equipos
+     * que continuan en el torneo
+     */
     fun mostrar(){
         for (item in equipos_listos){
             if(item is Team){
                 var temp = item as Team
-                println(temp.getNombre())
+                println(temp.obtenerNombre())
             }
-
         }
-
     }
+
 
     /**
      * Metodo que genera una habilidad de manera aleatoria
      * entre 50 y 400
+     * @return habilidad: habilidad que se le asignara al equipo
      */
     fun generaHabilidad(): Int{
         var random = Random()
@@ -51,95 +77,153 @@ class Torneo {
     }
 
     /**
-     *Funcion que enfrenta todos los equipos
+     *Funcion que enfrenta dos equipos y elimina al perdedor
+     * @param equipoA:  primer equipo en combatir
+     * @param equipoB: rival del equipoA
+     * @return 1: si gana el equipoA
+     * @return 2: si gana el equipoB
      */
-    fun jugarCuartos(time: Boolean){
-        if(time == true)
-            Thread.sleep(3_000)
-        var combate1 = Combate(equipos_listos[0], equipos_listos[1])
-        var ganador1: Int =  combate1.determinaGanador()
-        println("Ganador del primer combate 1 : $ganador1")
-        var combate2 = Combate(equipos_listos[2], equipos_listos[3])
-        var ganador2: Int =  combate2.determinaGanador()
-        println("Ganador del primer combate 2 : $ganador2")
-        var combate3 = Combate(equipos_listos[4], equipos_listos[5])
-        var ganador3: Int =  combate3.determinaGanador()
-        println("Ganador del primer combate 3 : $ganador3")
-        var combate4 = Combate(equipos_listos[6], equipos_listos[7])
-        var ganador4: Int =  combate4.determinaGanador()
-        println("Ganador del primer combate 4 : $ganador4")
-        if(ganador1==1)
-            equipos_listos.remove(equipos_listos[1])
-        else if (ganador1 == 2)
-            equipos_listos.remove(equipos_listos[0])
-        if(ganador2==1)
-            equipos_listos.remove(equipos_listos[2])
-        else if (ganador2 == 2)
-            equipos_listos.remove(equipos_listos[1])
-        if(ganador3==1)
-            equipos_listos.remove(equipos_listos[3])
-        else if (ganador3 == 2)
-        equipos_listos.remove(equipos_listos[2])
-        if(ganador4==1)
-        equipos_listos.remove(equipos_listos[4])
-        else if (ganador4 == 2)
-        equipos_listos.remove(equipos_listos[3])
+    fun realizarCombate(equipoA: Team, equipoB: Team): Int{
+        var combate = Combate(equipoA, equipoB)
+        var ganador: Int =  combate.determinaGanador()
+        println("Ganador del primer combate 1 : $ganador")
+        if(ganador==1)
+            equipos_listos.remove(equipoB)
+        else if (ganador == 2)
+            equipos_listos.remove(equipoA)
+        return ganador
+    }
+
+
+    /**
+     *Funcion que enfrenta todos los equipos
+     * y elimina a los perdedores
+     * @param combate: seleccion de los 4 combates disponibles
+     * @param seleccion: equipo apostado por el usuario
+     * @param cantidad: cantidad apostada por el usuario en esta ronda
+     * @return nombres: nombres de los ganadores por cada combate
+     */
+    fun jugarCuartos( combate:Int, seleccion: Int, cantidad: Int): Array<String>{
+
+        val habilidades: Array<Int> = arrayOf(equipo1.obtenerHabilidad(), equipo2.obtenerHabilidad(), equipo3.obtenerHabilidad(),equipo4.obtenerHabilidad(),
+                equipo5.obtenerHabilidad(), equipo6.obtenerHabilidad(), equipo7.obtenerHabilidad(), equipo8.obtenerHabilidad())
+        var nombres : Array<String>
+        var resultado : Boolean
+        var ganancia : Int = 0
+        var ganador1 = realizarCombate(equipos_listos[0], equipos_listos[1])
+        var ganador2 = realizarCombate(equipos_listos[1], equipos_listos[2])
+        var ganador3 = realizarCombate(equipos_listos[2], equipos_listos[3])
+        var ganador4 = realizarCombate(equipos_listos[3], equipos_listos[4])
+        if(combate>=1 && combate <=4 && seleccion >=1 && seleccion<=2 && cantidad>=1 && saldo_inicial>0 && saldo_inicial>=cantidad){
+            if (combate == 1){
+                resultado = ganador1 == seleccion
+                ganancia =  calcularGanancia(habilidades[0], habilidades[1], cantidad, seleccion,resultado)}
+            if (combate == 2) {
+                resultado = ganador2 == seleccion
+                ganancia =  calcularGanancia(habilidades[2], habilidades[3], cantidad, seleccion,resultado)}
+            if (combate == 3){
+                resultado = ganador2 == seleccion
+                ganancia =  calcularGanancia(habilidades[4], habilidades[5], cantidad, seleccion,resultado)}
+            if (combate == 4){
+                resultado = ganador2 == seleccion
+                ganancia =  calcularGanancia(habilidades[6], habilidades[7], cantidad, seleccion,resultado)}
+            saldo_generado += ganancia
+            saldo_inicial+=saldo_generado}
+        nombres = arrayOf(equipos_listos[0].obtenerNombre(), equipos_listos[1].obtenerNombre(), equipos_listos[2].obtenerNombre(), equipos_listos[3].obtenerNombre())
+        return nombres
     }
 
     /**
      *Funcion que enfrenta los 2 equipos restantes
+     * y elimina a los perdedores
+     *@param combate: seleccion de los 2 combates disponibles
+     * @param seleccion: equipo apostado por el usuario
+     * @param cantidad: cantidad apostada por el usuario en esta ronda
+     * @return nombres: nombres de los ganadores por cada combate
      */
-    fun jugarSemifinal(time: Boolean){
-        if(time == true)
-            Thread.sleep(3_000)
-        var combate1 = Combate(equipos_listos[0], equipos_listos[1])
-        var ganador1: Int =  combate1.determinaGanador()
-        println("Ganador del primer combate 1 : $ganador1")
-        var combate2 = Combate(equipos_listos[2], equipos_listos[3])
-        var ganador2: Int =  combate2.determinaGanador()
-        println("Ganador del primer combate 2 : $ganador2")
-        if(ganador1==1)
-            equipos_listos.remove(equipos_listos[1])
-        else if (ganador1 == 2)
-            equipos_listos.remove(equipos_listos[0])
-        if(ganador2==1)
-            equipos_listos.remove(equipos_listos[2])
-        else if (ganador2 == 2)
-            equipos_listos.remove(equipos_listos[1])
+    fun jugarSemifinal(combate:Int, seleccion: Int, cantidad: Int):Array<String>{
+        var nombres : Array<String>
+        val habilidades : Array<Int> = arrayOf(equipos_listos[0].obtenerHabilidad(), equipos_listos[1].obtenerHabilidad(),
+                equipos_listos[2].obtenerHabilidad(),equipos_listos[3].obtenerHabilidad())
+        var ganador1 = realizarCombate(equipos_listos[0], equipos_listos[1])
+        var ganador2 = realizarCombate(equipos_listos[1], equipos_listos[2])
+        var resultado : Boolean = false
+        var ganancia : Int = 0
+        if(combate>=1 && combate <=2 && seleccion >=1 && seleccion<=2 && cantidad>=1 && saldo_inicial>0 && saldo_inicial>=cantidad){
+            if (combate == 1){
+                resultado = ganador1 == seleccion
+                ganancia =  calcularGanancia(habilidades[0], habilidades[1], cantidad, seleccion,resultado)}
+            if (combate == 2) {
+                resultado = ganador2 == seleccion
+                ganancia =  calcularGanancia(habilidades[2], habilidades[3], cantidad, seleccion,resultado)}
+            saldo_generado += ganancia
+            saldo_inicial+=saldo_generado}
+        nombres = arrayOf(equipos_listos[0].obtenerNombre(), equipos_listos[1].obtenerNombre())
+        return nombres
     }
 
     /**
      *funcion que enfrenta a los dos equipos restantes
+     * y elimina al perdedor
+     * @param seleccion: equipo apostado por el usuario
+     * @param cantidad: cantidad apostada por el usuario en esta ronda
+     * @return nombres: nombres de los ganadores por cada combate
      */
-    fun jugarFinal(time: Boolean){
-        if(time == true)
-            Thread.sleep(3_000)
-        var combate1 = Combate(equipos_listos[0], equipos_listos[1])
-        var ganador1: Int =  combate1.determinaGanador()
-        println("Ganador del torneo es : $ganador1")
-        if(ganador1==1)
-            equipos_listos.remove(equipos_listos[1])
-        else if (ganador1 == 2)
-            equipos_listos.remove(equipos_listos[0])
+    fun jugarFinal( seleccion: Int, cantidad: Int): Array<String>{
+        var nombres : Array<String>
+        val habilidades : Array<Int> = arrayOf(equipos_listos[0].obtenerHabilidad(), equipos_listos[1].obtenerHabilidad())
+        var ganador1 = realizarCombate(equipos_listos[0], equipos_listos[1])
+        var resultado : Boolean = false
+        var ganancia : Int = 0
+        if(seleccion >=1 && seleccion<=2 && cantidad>=1 && saldo_inicial>0 && saldo_inicial>=cantidad){
+            resultado = ganador1 == seleccion
+            ganancia =  calcularGanancia(habilidades[0], habilidades[1], cantidad, seleccion,resultado)
+            saldo_generado += ganancia}
+        saldo_inicial+=saldo_generado
+        nombres = arrayOf(equipos_listos[0].obtenerNombre())
+        return nombres
+    }
+
+    /**
+     * Funcion que devuelve a los jugadores activos
+     * @return jugadores: jugadores activos
+     */
+    fun getJugadores(): MutableList<Team>{
+        return equipos_listos
+    }
+
+    /**
+     *Funcion que calcula la gananancia del usuario si apuesta en un torneo
+     * la ganancia puede ser positiva o negativa
+     * @param equipo1  equipos que se enfrentaran
+     * @param equipo2 equipos que se enfrentan
+     * @param cantidad cantidad apostada por el usuario
+     * @param seleccion equipo apostado por el usuario
+     * @param resultado resultado del combate entre los dos equipos
+     * @return ganancia: ganancia del usuario al apostar por cierto equipo
+     */
+    fun calcularGanancia(habilidad_equipo1: Int, habilidad_equipo2: Int, cantidad: Int,seleccion: Int, resultado: Boolean): Int{
+        var apuesta = Apuesta(habilidad_equipo1, habilidad_equipo2)
+        var cuota = apuesta.calcularCuota(seleccion)
+        var ganancia: Int
+        if (resultado == true)
+            ganancia = apuesta.calculaGanancia(cantidad, seleccion)
+        else
+            ganancia = -cantidad
+        return ganancia
+    }
+
+    /**
+     * Funcion que devuelve el saldo que se ha generado durante toda la partida
+     */
+    fun obtenerSaldoGenerado():Int{
+        return  saldo_generado
     }
 
 }
 
+class TorneoModel(torneo: Torneo): ItemViewModel<Torneo>(torneo) {
 
-
-
-
-fun main(args: Array<String>){
-
-    var test = Torneo()
-    test.organizaCombates()
-    test.mostrar()
-    test.jugarCuartos(true)
-    test.mostrar()
-    test.jugarSemifinal(true)
-    test.mostrar()
-    test.jugarFinal(true)
-    test.mostrar()
-    print("\nFinish")
-
+    val saldo_generado = bind(Torneo::saldo_generadoProperty)
+    
 }
